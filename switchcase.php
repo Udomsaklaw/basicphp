0c46fa35dc298e30415cfa045320abac4a03afbe
Udomsak Padpon <upadpon@gmail.com>
2019-05-09 10:04:36 +0700

    Save Project no.1

---
+++ switchcase.php
@@ -0,0 +1,21 @@
+<?php
+$size = "s";
+
+switch ($size)
+{
+    case 's':
+        echo "Small Size";
+        break;
+
+    case 'm':
+        echo "Meiduim Size";
+        break;
+
+    case 'l':
+        echo "Large Size";
+        break;
+
+    default:
+        echo "Unknow Size";
+        break;
+}
